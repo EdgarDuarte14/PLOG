dcd3ad48588f4e38aea52f18eb927dcff66fde4e
EdgarDuarte14 <up201305973@fe.up.pt>
2015-11-04 19:02:42 +0000

    check path started

--- src/distrify.pl
+++ src/distrify.pl
@@ -12,12 +12,9 @@ distrify:-
 
 
 %---------> Change the player <---------%
-changePlayer(1, NextPlayer):-
-	NextPlayer = 2.
-
-changePlayer(2, NextPlayer):-
-	NextPlayer = 1.
-
+changePlayer(Player, NextPlayer):-
+	(Player = 1 -> NextPlayer = 2;
+	Player = 2 -> NextPlayer = 1). 
 
 %--------> Main game program <-----------%
 
@@ -53,3 +50,55 @@ update(Move, Value, (Move1, Value1), (Move1, Value1)):-
 
 update(Move, Value, (Move1, Value1), (Move, Value)):-
 	Value > Value1.
+
+%-------------> Check path <---------------%
+
+%Check the neighbors of a point
+
+neighbor(Line, Column, Line1, Column1):-
+	Column1 is Column + 1,
+	Line1 is Line + 1,
+	Line1 =< 8,
+	Column1 =< 8.
+
+
+neighbor(Line, Column, Line1, Column1):-
+	Column1 is Column + 1,
+	Line1 is Line - 1,
+	Line1 >= 0,
+	Column1 =< 8.
+
+
+neighbor(Line, Column, Line1, Column1):-
+	Column1 is Column - 1,
+	Line1 is Line - 1,
+	Line1 >= 0,
+	Column1 >= 0.
+
+
+neighbor(Line, Column, Line1, Column1):-
+	Column1 is Column - 1,
+	Line1 is Line + 1,
+	Line1 =< 8,
+	Column1 >= 0.
+
+
+neighbor(Line, Column, Line1, Column1):-
+	Column1 is Column + 1,
+	Column1 =< 8.
+
+
+neighbor(Line, Column, Line1, Column1):-
+	Column1 is Column - 1,
+	Column1 >= 0.
+
+
+neighbor(Line, Column, Line1, Column1):-
+	Line1 is Line + 1,
+	Line1 =< 8.
+
+
+neighbor(Line, Column, Line1, Column1):-
+	Line1 is Line - 1,
+	Line1 >= 0.
+
