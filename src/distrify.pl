23079b6341f0475b866eab3bf11637a78a9f9b5d
EdgarDuarte14 <up201305973@fe.up.pt>
2015-11-04 19:40:39 +0000

    end of path and check path, to modifie

--- src/distrify.pl
+++ src/distrify.pl
@@ -18,6 +18,10 @@ changePlayer(Player, NextPlayer):-
 
 %--------> Main game program <-----------%
 
+
+% Codigo do prof %
+
+
 play(Game):-
 	initialize(Game, Position, Player),
 	display_game(Position, Player),
@@ -51,9 +55,17 @@ update(Move, Value, (Move1, Value1), (Move1, Value1)):-
 update(Move, Value, (Move1, Value1), (Move, Value)):-
 	Value > Value1.
 
+% Codigo do prof %
+
+
+
+
+
+
+
 %-------------> Check path <---------------%
 
-%Check the neighbors of a point
+% Verificar os vizinhos de um ponto %
 
 neighbor(Line, Column, Line1, Column1):-
 	Column1 is Column + 1,
@@ -101,4 +113,48 @@ neighbor(Line, Column, Line1, Column1):-
 neighbor(Line, Column, Line1, Column1):-
 	Line1 is Line - 1,
 	Line1 >= 0.
+
+
+
+% Criar um caminho %
+
+path([[Line,Column] | T], Board, Player):-
+	neighbor(Line, Column, Line1, Column1),
+	once(getPiece(Line1, Column1, Board, Piece)),
+	Piece == Player,
+	\+ member([Line1, Column1], T),
+	path([[Line,Column] | [[Line, Column] | T]], Board, Player).
+ 
+% condicoes finais ---------- modificar % 
+path([[Line,8] | _], Board, white):-
+    getPiece(Line,8, Board, Piece),
+    Piece == white.
+
+path([[8,Column] | _], Board, black):-
+    getPiece(8,Column, Board, Piece),
+    Piece == black.
+
+
+% Tentativa de criar um caminho completo, tanto para o jogador 1 como para o jogador 2 %
+
+checkCompletePath(9, _, _, black).
+checkCompletePath(_, 9, _, white).
+
+checkCompletePath(Line, Column, Board, white):-
+	Line =< 8,
+	Column =< 8,
+	getPiece(Line, Column, Board, Piece),
+	(Piece \== white -> Line1 is Line + 1, checkCompletePath(Line1, Column, Board, white);
+		(\+ path([[Line, Column]] Board, white) -> Line1 is Line + 1, checkCompletePath(Line1, Column, Board, white))).
+
+
+
+checkCompletePath(Line, Column, Board, black):-
+	Line =< 8,
+	Column =< 8,
+	getPiece(Line, Column, Board, Piece),
+	(Piece \== black -> Line1 is Line + 1, checkCompletePath(Line1, Column, Board, black);
+		(\+ path([[Line, Column]] Board, black) -> Line1 is Line + 1, checkCompletePath(Line1, Column, Board, black);
+			true)).
+
 
