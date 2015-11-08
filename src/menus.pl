cda34fff613ca718c52167ce107731790531ca18
EdgarDuarte14 <up201305973@fe.up.pt>
2015-11-08 23:27:05 +0000

    done

--- src/menus.pl
+++ src/menus.pl
@@ -128,6 +128,40 @@ helpMenu:-
 	write('======================================================================'), nl,
 	pressEnter, nl.
 
+
+helpMenu:-
+	write('                                                                         '), nl,
+	write('                         ===> How to play <===                           '), nl,
+	write('                                                                         '), nl,
+	write('========================================================================='), nl,
+	write('========================================================================='), nl,
+	write('==                                                                     =='), nl,
+	write('==   Distrify is connection game played by two players, white and      =='), nl,
+	write('==   black pieces, the player with the black pieces (Player1) starts   =='), nl,
+	write('==   the game by putting a piece anywhere in the board, then the       =='), nl,
+	write('==   players start alternating turns and have a choice to place one    =='), nl,
+	write('==   or two stones, if two are placed:                                 =='), nl,
+	write('==   1) the placed stones must not be diagonally adjacent to each      =='), nl,
+	write('==   other                                                             =='), nl,
+    write('==   2) the placed stones cannot result in the formation of a new      =='), nl,
+    write('==   horizontal, vertical, or diagonal three-in-a-row of the same type =='), nl,
+    write('==                                                                     =='), nl,
+    write('==   a player move can never result in a crosscut                      =='), nl,
+    write('==   CROSSCUT: X 0                                                     =='), nl,
+    write('==             0 X                                                     =='), nl,
+    write('==                                                                     =='), nl,
+    write('==   Black wins if there exists a path of orthogonally or diagonally   =='), nl,
+    write('==   adjacent black stones reaching between the top and bottom rows    =='), nl,
+    write('==   while White wins if there exists an analogous path stretching     =='), nl,
+    write('==   from the westmost to eastmost columns.                            =='), nl,
+    write('==                                                                     =='), nl,
+    write('========================================================================='), nl,
+    write('========================================================================='), nl,
+    pressEnter, nl.
+
+
+
+
 aboutGameMenu:-
 	clearScreen,
 	write('                                     '), nl,
