f0df09e2c0611c02e3c57454899acd6cafdf33c5
ElTostas <andre12.1@hotmail.com>
2015-11-08 12:01:45 +0000

    bots e ciclos todos feitos

--- src/print.pl
+++ src/print.pl
@@ -1,15 +1,15 @@
 :-use_module(library(lists)).
 :-use_module(library(random)).
 
-starting_state([[0,1,0,0,0,0,0,0,0],
-                [0,1,0,0,0,0,0,0,0],
-                [2,2,1,2,0,0,1,0,0],
-                [0,1,0,0,0,0,0,0,0],
-                [0,1,0,0,0,0,0,0,0],
-                [1,2,0,2,0,2,0,2,0],
-                [2,1,0,0,0,0,0,0,0],
+starting_state([[0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
-                [0,1,0,0,0,0,0,0,0]
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0]
                 ]).
 
 
@@ -37,8 +37,11 @@ show_board(Board):-
     nl, printColumnId, printInitialSeperator,
     show_lines(1, Board), nl.
 
+startHvH:-starting_state(Board), show_board(Board), humanVhuman(Board, 1).
 
-start:- starting_state(Board), show_board(Board), setRandomPieces(Board, 10, NewBoard), show_board(NewBoard) , humanVhuman(NewBoard, 1).
+startCvsH:- starting_state(Board), show_board(Board), computerVhuman(Board, 1, 1, Line_store, Column_store).
+
+startCvsC:-starting_state(Board), show_board(Board), random(1,10, Column),  computerVcomputer(Board,1, 0, Column, Line_store, Column_store).
 
 
 
@@ -355,7 +358,7 @@ checkCompletePath(10, _, _, 1).
 checkCompletePath(_, 10, _, 2).
 
 checkCompletePath(Line, Column, Board, 2):-
-    write('checkCompletePath'),
+   %write('checkCompletePath'),
     Line =< 9,
     Column =< 9,
     getCell(Line, Column, Board, Piece),
@@ -365,7 +368,7 @@ checkCompletePath(Line, Column, Board, 2):-
 
 
 checkCompletePath(Line, Column, Board, 1):- 
-    write('checkCompletePath'),
+   %write('checkCompletePath'),
     nl,
     Line =< 9,
     Column =< 9,
@@ -377,4 +380,150 @@ checkCompletePath(Line, Column, Board, 1):-
 
 
 
-%decide_white_play(Board, Line, Column, Line1, Column1, NrPieces):-
+
+simulate_play(Board, Line, Column, Piece):-
+    getCell(Line, Column, Board, Cell),
+    Cell = 0,
+    check_crosscut(Board, Line, Column, Piece).
+
+simulate_play(Board, Line, Column, Line1, Column1 ,  Piece, Dir):-
+    getCell(Line, Column, Board, Cell),
+    Cell = 0,
+    check_crosscut(Board, Line, Column, Piece),
+    getCell(Line1, Column1, Board, Cell1),
+    Cell1 = 0,
+    check_three_in-row(Board, Dir, Piece, Line1, Column1), 
+    check_crosscut(Board, Line1, Column1, Piece).
+
+
+computer_set(Board, Result, Piece, Line, Column, Line1, Column1):-
+    setCell(Line, Column, Board, Piece, Result1, Sucess),
+    setCell(Line1, Column1, Result1, Piece, Result, Sucess).
+
+computer_set(Board, Result, Piece, Line, Column):-
+    setCell(Line, Column, Board, Piece, Result, Sucess).
+
+
+
+decide_white_play(Board, Line, 0, Result, Line_store, Column_store):-
+    computer_set(Board, Result, 2, Line, 1),
+    Line_store = Line, 
+    Column_store = 1,
+    !.
+
+
+decide_white_play(Board, Line, Column, Result, Line_store, Column_store):-
+    Column1 is Column+1,
+    Column2 is Column+2,
+    Line1 is Line+1,
+    write('Column1 : '), write(Column1), nl, write('Column2 :'), write(Column2),nl, write('Line1: '), write(Line1),nl,
+    ( (write('OP1'),simulate_play(Board, Line1, Column1, Line1, Column2, 2, 3) ,  Line1 =< 9, Column2 =< 9 )-> write('pintou 2'), computer_set(Board, Result, 2, Line1, Column1, Line1, Column2), Line_store = Line1, Column_store = Column2,  !; 
+        (write('OP2'),Line2 is Line-1 , (simulate_play(Board, Line2, Column1, Line2, Column2, 2 , 3), Line2 > 0 , Column2 =< 9 )->  computer_set(Board, Result, 2, Line2, Column1, Line2, Column2), Line_store = Line2, Column_store = Column2, ! ; 
+            ( (write('OP3'),simulate_play(Board, Line, Column1, 2) , Column1 =< 9 )-> computer_set(Board, Result, 2, Line, Column1) , Line_store = Line, Column_store = Column1, ! ; 
+                ( (write('OP4'),simulate_play(Board, Line1, Column1, 2), Line1 =< 9 )-> computer_set(Board, Result, 2, Line1, Column1) , Line_store = Line1, Column_store = Column1,! ; 
+                    ( (write('OP5'),Line2 is Line-1 ,simulate_play(Board, Line2, Column2, 2), Line2 > 0 , Column2 =< 9  )-> computer_set(Board, Result, 2, Line2, Column2) , write('chega aqui'), Line_store = Line2, Column_store = Column2, ! ; 
+                        (write('OP6'),Column3 is Column-1, Line3 is Line+2, write('COLUMN3: '),write(Column3), ( simulate_play(Board, Line1, Column3, Line3, Column3, 2, 2), Column3 > 0 , Line3 =< 9 )-> computer_set(Board, Result, 2, Line1, Column3, Line3, Column3), Line_store = Line3, Column_store = Column3, !;
+                            ( (write('OP7'), Column3 is Column-1,simulate_play(Board, Line1, Column3, 2), Column3 > 0 )-> computer_set(Board, Result,2, Line1, Column3), Line_store = Line1, Column_store = Column3, write('saiu'),! ; 
+                                write('OP8'),decide_white_play_rand(Board, Result, Line_store, Column_store)))))))).
+
+decide_black_play(Board, 0, Column, Result, Line_store, Column_store):-
+    write('entrou primeira'),
+    computer_set(Board, Result, 1, 1, Column),
+    Line_store = 1, 
+    Column_store = Column,
+    !.
+
+decide_black_play(Board, Line, Column, Result, Line_store, Column_store):-
+    Line1 is Line+1,
+    Line2 is Line+2,
+    Column1 is Column+1,
+    %write('Column1 : '), write(Column1), nl, write('Column2 :'), write(Column2),nl, write('Line1: '), write(Line1),nl,
+    ( ( write('OP1'),simulate_play(Board, Line1, Column1, Line2, Column1, 1, 2) ,  Column1 =< 9, Line2 =< 9 )-> write('pintou 2'), computer_set(Board, Result, 1, Line1, Column1, Line2, Column1), Line_store = Line2, Column_store = Column1,  !; 
+        (write('OP2') ,Column2 is Column-1 , (simulate_play(Board, Line1, Column2, Line2, Column2, 1 , 2), Column2 > 0 , Line2 =< 9 )->  computer_set(Board, Result, 1, Line1, Column2, Line2, Column2), Line_store = Line2, Column_store = Column2, ! ; 
+            ( (write('OP3'),simulate_play(Board, Line1, Column, 1) , Line1 =< 9 )-> computer_set(Board, Result, 1, Line1, Column) , Line_store = Line1, Column_store = Column, ! ; 
+                ( (write('OP4'),simulate_play(Board, Line1, Column1, 1), Line1 =< 9 )-> computer_set(Board, Result, 1, Line1, Column1) , Line_store = Line1, Column_store = Column1,! ; 
+                    ( (write('OP5'),Column2 is Column-1 ,simulate_play(Board, Line2, Column2, 1), Column2 > 0 , Line2 =< 9  )-> computer_set(Board, Result, 1, Line2, Column2) , Line_store = Line2, Column_store = Column2, ! ; 
+                        (write('OP6'),Line3 is Line-1,write('LINE3: '),write(Line3), Column3 is Column+2,  ( simulate_play(Board, Line3, Column1, Line3, Column3, 1, 3), Line3 > 0 , Column3 =< 9 )-> computer_set(Board, Result, 1, Line1, Column3, Line3, Column3), Line_store = Line3, Column_store = Column3, !;
+                            ( ( write('OP7'),Line3 is Line-1,simulate_play(Board, Line3, Column1, 1), Line3 > 0 )-> computer_set(Board, Result,1, Line3, Column1), Line_store = Line3, Column_store = Column1,! ; 
+                                write('OP8'),decide_black_play_rand(Board, Result, Line_store, Column_store)))))))).
+
+decide_black_play_rand(Board, Result, Line_store, Column_store):-
+    random(1, 10, Line),
+    random(1,10, Column),
+    write(Line), write('RAND'),
+    write(Column),
+    ( simulate_play(Board, Line, Column, 1)-> computer_set(Board, Result, 1, Line, Column) , Line_store = Line, Column_store = Column; decide_black_play_rand(Board, Result, Line_store, Column_store)).
+
+decide_white_play_rand(Board, Result, Line_store, Column_store):-
+    random(1, 10, Line),
+    random(1,10, Column),
+     write(Line), write('RAND'),
+    write(Column),
+    ( simulate_play(Board, Line, Column, 2)-> computer_set(Board, Result, 2, Line, Column) , Line_store = Line, Column_store = Column; decide_white_play_rand(Board, Result, Line_store, Column_store)).
+
+
+
+
+
+computerVhuman(Board, 1, First, Line_store, Column_store):-
+    write('JOGADOR1: '), nl,
+    write('nr de pecas a jogar (1 ou 2): '),
+    read(N),
+    human_play(1, N,  Line, Column, Board , Board1),
+    show_board(Board1),
+    (checkCompletePath(1,1,Board1,1) -> write('JOGADOR'), write(1), write(' VENCEU O JOGO'), ! ; (First =:= 1 -> random(1, 10, Column1), computerVhuman(Board1, 2, 0,Column1) ; computerVhuman(Board1, 2, Line_store,Column_store)) ).
+
+computerVhuman(Board, 2, Line, Column):-
+    write('COMPUTADOR'), nl,
+    decide_white_play(Board, Line, Column, Result, Line_store, Column_store),
+    write(Line_store),
+    write(Column_store),
+    show_board(Result),
+    (checkCompletePath(1,1,Result, 2) -> write('COMPUTADOR VENCEU O JOGO!') , ! ; computerVhuman(Result, 1 , 0, Line_store, Column_store)).
+
+
+
+
+
+
+
+computerVcomputer(Board, 1, 9, Column, Line_store_w, Column_store_w):-
+    nl, write(Column),nl,write(Line_store_w),nl,write(Column_store_w),
+     write('COMPUTADOR1'),nl,
+     decide_black_play_rand(Board, Result, Line_store, Column_store) ,
+     show_board(Result), 
+     (checkCompletePath(1,1,Result,1) -> write('COMPUTADOR'), write(1), write(' VENCEU O JOGO'), ! ;
+         computerVcomputer(Result,2, Line_store_w, Column_store_w, Line_store, Column_store) ),!.
+
+
+computerVcomputer(Board, 1, Line, Column, Line_store_w, Column_store_w):-
+write(Line),nl, write(Column),nl,write(Line_store_w),nl,write(Column_store_w),
+ write(Line),nl, write(Column),
+    write('COMPUTADOR1'),nl,
+    random(0,5,Rand),
+    (Rand < 4 -> decide_black_play(Board, Line, Column, Result, Line_store, Column_store), show_board(Result) ,  (checkCompletePath(1,1,Result,1) -> write('COMPUTADOR'), write(1), write(' VENCEU O JOGO'), ! ; computerVcomputer(Result,2, Line_store_w, Column_store_w, Line_store, Column_store)) ; 
+        decide_black_play_rand(Board, Result, Line_store, Column_store) ,show_board(Result), (checkCompletePath(1,1,Result,1) -> write('COMPUTADOR'), write(1), write(' VENCEU O JOGO'), ! ;
+         computerVcomputer(Result,2, Line_store_w, Column_store_w, Line_store, Column_store) )  ).
+
+
+
+
+computerVcomputer(Board, 2, Line, 9, Line_store_b, Column_store_b):-
+    write(Line),nl,write(Line_store_b),nl,write(Column_store_b),
+     write('COMPUTADOR2'),nl,
+     decide_white_play_rand(Board, Result, Line_store, Column_store) ,
+     show_board(Result), 
+     (checkCompletePath(1,1,Result,2) -> write('COMPUTADOR'), write(2), write(' VENCEU O JOGO'), ! ;
+         computerVcomputer(Result,1, Line_store_b, Column_store_b, Line_store, Column_store) ),!.
+
+computerVcomputer(Board, 2, Line, Column, Line_store_b, Column_store_b):-
+    write(Line),nl, write(Column),nl,write(Line_store_b),nl,write(Column_store_b),
+    write('COMPUTADOR2'),nl,
+    random(0,5,Rand),
+    (Rand < 4 -> write('not rand'), decide_white_play(Board, Line, Column, Result, Line_store, Column_store) ,show_board(Result),  (checkCompletePath(1,1,Result,2) -> write('COMPUTADOR'), write(2), write(' VENCEU O JOGO'), ! ; computerVcomputer(Result,1, Line_store_b, Column_store_b, Line_store, Column_store)) ; 
+       write(' rand'), decide_white_play_rand(Board, Result, Line_store, Column_store) ,show_board(Result), (checkCompletePath(1,1,Result,2) -> write('COMPUTADOR'), write(2), write(' VENCEU O JOGO'), ! ;
+         computerVcomputer(Result,1, Line_store_b, Column_store_b, Line_store, Column_store) ) ).
+
+
+
+
